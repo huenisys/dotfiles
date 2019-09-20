6dd2e7d4b1d8f9cf32ea3ea4fd938903cfc7598f
huenisys <paul@huenits.com>
2019-09-20 15:07:58 +0800

    set php to 7.3

--- run1-install-php-and-composer.sh
+++ run1-install-php-and-composer.sh
@@ -3,8 +3,8 @@
 cd
 
 ## php
-sudo apt install -y php7.2-cli
-sudo apt install -y nginx php-memcached php-msgpack php-igbinary $(php_pack=(fpm cli gd mysql pgsql imap mbstring xml curl bcmath sqlite3 soap intl readline imagick zip yaml);printf 'php7.2-%s ' "${php_pack[@]}")
+sudo apt install -y php7.3-cli
+sudo apt install -y nginx php-memcached php-msgpack php-igbinary $(php_pack=(fpm cli gd mysql pgsql imap mbstring xml curl bcmath sqlite3 soap intl readline imagick zip yaml);printf 'php7.3-%s ' "${php_pack[@]}")
 
 ## composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
