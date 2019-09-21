4f0926be1a24a2a46c53662716be52630426eee0
huenisys <paul@huenits.com>
2019-09-22 03:07:22 +0800

    shared composer

--- run3-install-php-and-composer.sh
+++ run3-install-php-and-composer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## vars
+COMPOSER_HOME="/usr/local/share/composer"
+
 cd
 
 ## php
@@ -7,6 +10,15 @@ sudo apt install -y php7.2-cli
 sudo apt install -y curl zip unzip sqlite3 nginx php-memcached php-msgpack php-igbinary $(php_pack=(fpm cli gd mysql pgsql imap mbstring xml curl bcmath sqlite3 soap intl readline imagick zip yaml xdebug);printf 'php7.2-%s ' "${php_pack[@]}")
 
 ## composer
+
+sudo addgroup composer_users
+#sudo usermod -aG composer_users huenisys
+sudo rm -rf $COMPOSER_HOME
+sudo mkdir $COMPOSER_HOME
+sudo chgrp composer_users $COMPOSER_HOME
+sudo chmod g+sw,u+s $COMPOSER_HOME
+echo "export COMPOSER_HOME='/usr/local/share/composer'" | sudo tee -a /etc/profile
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php composer-setup.php --filename=composer
+sudo mv composer /usr/local/bin/
 php -r "unlink('composer-setup.php');"
